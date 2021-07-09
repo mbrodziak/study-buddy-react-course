cc9abd588d4625f37270a494f5a40e08f1517efb
Mateusz Brodziak <Mateusz@Mateusz>
2021-07-09 22:08:25 +0200

    Add task from episode 22 about redux

--- src/views/Notes.js
+++ src/views/Notes.js
@@ -4,22 +4,27 @@ import Note from 'components/molecules/Note/Note';
 import { useDispatch, useSelector } from 'react-redux';
 import { addNote } from 'store';
 import { FormWrapper, NotesWrapper, StyledFormField, Wrapper } from 'views/Notes.styles';
+import { useForm } from 'react-hook-form';
 
 const Notes = () => {
   const notes = useSelector((state) => state.notes);
   const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => handleAddNote(data);
 
-  const handleAddNote = () => {
-    dispatch(addNote({ title: `New Note ${Math.floor(Math.random() * 10)}`, content: 'Lorem ipsum dolor sit amet' }));
+  const handleAddNote = ({ title, content }) => {
+    dispatch(addNote({ title: title, content: content }));
   };
 
   return (
     <Wrapper>
-      <FormWrapper>
-        <StyledFormField label="Title" name="title" id="title" />
-        <StyledFormField isTextarea label="Content" name="content" id="content" />
-        <Button onClick={handleAddNote}>Add</Button>
-      </FormWrapper>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormWrapper>
+          <StyledFormField label="Title" name="title" id="title" {...register('title')} />
+          <StyledFormField isTextarea label="Content" name="content" id="content" {...register('content')} />
+          <Button type="submit">Add</Button>
+        </FormWrapper>
+      </form>
       <NotesWrapper>
         {notes.length ? (
           notes.map(({ title, content, id }) => <Note id={id} key={id} title={title} content={content} />)
